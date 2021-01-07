a024a520835dff7fd3ccf151530dd7e13df9c8a9
Jeremy <77029343+asapjezza@users.noreply.github.com>
2021-01-07 18:56:10 +0800

    Update Robot.cpp
    Create motor control member function's implementations

--- 4788/src/main/cpp/Robot.cpp
+++ 4788/src/main/cpp/Robot.cpp
@@ -3,74 +3,101 @@
 using namespace frc;
 using namespace wml;
 
-using hand = frc::XboxController::JoystickHand;
+using Hand = frc::XboxController::JoystickHand;
 
-double currentTime;
-double lastTimeStamp;
-double dt; //stands for delta time 
+const double deadzone = 0.1;
 
-//add other variables here
-double sparkSpeed;
-double talonSpeed;
-double constexpr deadzone = 0.1;
+void Robot::RobotInit()
+{
+	controller = new frc::XboxController(0);
+	victors = new wml::VictorSpx[2] {wml::VictorSpx(9), wml::VictorSpx(8)};
+	talons = new wml::TalonSrx[2] {wml::TalonSrx(2), wml::TalonSrx(1)};
 
-// Robot Logic
-void Robot::RobotInit() {
-	//init controllers 
-	xbox = new frc::XboxController(0);
+	victors[1].SetInverted(true);
+	talons[1].SetInverted(true);
 
-	//Motor examples 
-	_sparkMotor = new frc::Spark(0);
-	_talonMotor = new wml::TalonSrx(1);
-
-	_sparkMotor->SetInverted(true);
-	_talonMotor->SetInverted(false);
 }
 
-void Robot::RobotPeriodic() {}
-
-// Dissabled Robot Logic
-void Robot::DisabledInit() {}
-void Robot::DisabledPeriodic() {}
-
-// Auto Robot Logic
-void Robot::AutonomousInit() {}
-void Robot::AutonomousPeriodic() {}
-
-// Manual Robot Logic
-void Robot::TeleopInit() {}
-void Robot::TeleopPeriodic() {
-	currentTime = Timer::GetFPGATimestamp();
-	dt = currentTime - lastTimeStamp;
-
-	//motor examples
-	sparkSpeed = xbox->GetY(hand::kLeftHand);
-	_sparkMotor->Set(sparkSpeed);
-
-
-	talonSpeed = xbox->GetTriggerAxis(hand::kRightHand);
-	if (talonSpeed >= deadzone) { //acounts for the deadzone
-		_talonMotor->Set(talonSpeed);
-	} else {
-		_talonMotor->Set(0);
-	}
-
-	// ^ the equivilant using a conditional statement 
-	//talonSpeed = xbox->GetTriggerAxis(hand::kRightHand) > deadzone ? xbox->GetTriggerAxis(hand::kRightHand) : 0; _talonMotor->Set(talonSpeed);
-
-	if(xbox->GetXButton()) {
-		_solenoid.SetTarget(wml::actuators::BinaryActuatorState::kForward);
-	} else {
-		_solenoid.SetTarget(wml::actuators::BinaryActuatorState::kReverse);
-	}
-
-	_compressor.Update(dt);
-	_solenoid.Update(dt);
-
-	if (_solenoid.IsDone()) _solenoid.Stop();
-	lastTimeStamp = currentTime;
+void Robot::RobotPeriodic()
+{
 }
 
-// Test Logic
-void Robot::TestInit() {}
-void Robot::TestPeriodic() {}
+void Robot::DisabledInit()
+{
+}
+
+void Robot::DisabledPeriodic()
+{
+}
+
+void Robot::AutonomousInit()
+{
+}
+
+void Robot::AutonomousPeriodic()
+{
+}
+
+void Robot::TeleopInit()
+{
+}
+
+void Robot::TeleopPeriodic()
+{
+	if (std::abs(left_power) >= deadzone) {
+		left_power = controller->GetY(Hand::kLeftHand);
+	} 
+	else {
+		left_power = 0;
+	}
+	if (std::abs(right_power) >= deadzone) {
+		right_power = controller->GetY(Hand::kRightHand);
+	} 
+	else {
+		right_power = 0;
+	}
+
+	motors(right_power, left_power);
+}
+
+void Robot::TestInit()
+{
+}
+
+void Robot::TestPeriodic()
+{
+}
+
+// motors: set the motors from right power and left power
+void Robot::motors(const double right_power, const double left_power)
+{
+	talons[0].Set(right_power);
+	victors[0].Set(right_power);
+	talons[1].Set(left_power);
+	victors[1].Set(left_power);
+}
+
+// longitudinal: translate the robot longitudinally for a distance
+void Robot::longitudinal(const Meter& distance)
+{
+	motors(0, 0);
+
+	talons[0].ZeroEncoder();
+	talons[1].ZeroEncoder();
+
+	double rotations = (distance / wheel_circumference);
+
+	// while not at destination translate
+	if (distance > 0) {
+		while (((talons[0].GetEncoderRotations() + talons[1].GetEncoderRotations()) / 2) < rotations) {
+			motors(0.5, 0.5);
+		}
+	}
+	if (distance < 0) {
+		while (((talons[0].GetEncoderRotations() + talons[1].GetEncoderRotations()) / 2) < rotations) {
+			motors(-0.5, -0.5);
+		}
+	}
+
+	motors(0, 0);
+}
