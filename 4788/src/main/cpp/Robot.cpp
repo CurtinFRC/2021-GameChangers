d6f090687ced43ab26c5caf2dcf649e2bb260e6f
Viola <violablanducci@gmail.com>
2021-02-21 04:49:55 +0000

    Added drive train, Connors code merged and building but not tested

--- 4788/src/main/cpp/Robot.cpp
+++ 4788/src/main/cpp/Robot.cpp
@@ -19,42 +19,44 @@ void Robot::RobotInit() {
   // Drivebase Stuff
 
 	//Create wml drivetrain
-	// drivetrain = new Drivetrain(robotMap.driveSystem.drivetrainConfig, robotMap.driveSystem.gainsVelocity);
+	drivetrain = new Drivetrain(robotMap.driveSystem.drivetrainConfig, robotMap.driveSystem.gainsVelocity);
 
 	//Zero Encoders
-	// robotMap.driveSystem.drivetrain.GetConfig().leftDrive.encoder->ZeroEncoder();
-	// robotMap.driveSystem.drivetrain.GetConfig().rightDrive.encoder->ZeroEncoder();
+	robotMap.driveSystem.drivetrain.GetConfig().leftDrive.encoder->ZeroEncoder();
+	robotMap.driveSystem.drivetrain.GetConfig().rightDrive.encoder->ZeroEncoder();
 
 	//Strategy controllers (Set default strategy for drivetrain to be Manual)
-	// drivetrain->SetDefault(std::make_shared<DrivetrainManual>("Drivetrain Manual", *drivetrain, robotMap.contGroup));
-	// drivetrain->StartLoop(100);
+	drivetrain->SetDefault(std::make_shared<DrivetrainManual>("Drivetrain Manual", *drivetrain, robotMap.contGroup));
+	drivetrain->StartLoop(100);
 
 	//Inverts one side of our drivetrain
-	// drivetrain->GetConfig().rightDrive.transmission->SetInverted(true);
-	// drivetrain->GetConfig().leftDrive.transmission->SetInverted(false);
+	drivetrain->GetConfig().rightDrive.transmission->SetInverted(true);
+	drivetrain->GetConfig().leftDrive.transmission->SetInverted(false);
 
-	//Register our systems to be called via strategy
-	// StrategyController::Register(drivetrain);
-	StrategyController::Register(magazine);
 
 	// Climber
 	climber = new Climber(robotMap.climber.climberMotor);
 	climber->SetDefault(std::make_shared<ClimberManualStrategy>("Climber Manual", *climber, robotMap.contGroup));
 
-	// Register our systems to be called via strategy
-	// StrategyController::Register(drivetrain);
+
+	//Register our systems to be called via strategy
+	StrategyController::Register(drivetrain);
+	StrategyController::Register(magazine);
 	StrategyController::Register(climber);
-	// NTProvider::Register(drivetrain);
+
+	// Nt Provider register
+	NTProvider::Register(drivetrain);
 }
+
 
 void Robot::RobotPeriodic() {
   currentTimeStamp = frc::Timer::GetFPGATimestamp();
   dt = currentTimeStamp - lastTimeStamp;
 
 	StrategyController::Update(dt);
-	climber->update(dt);
 	NTProvider::Update();
-
+	
+	climber->update(dt);
   magazine->update(dt);
 
   lastTimeStamp = currentTimeStamp;
@@ -70,16 +72,11 @@ void Robot::AutonomousPeriodic() {}
 
 // Manual Robot Logic
 void Robot::TeleopInit() {
-<<<<<<< HEAD
   Schedule(magazine->GetDefaultStrategy(), true);
-	// Schedule(drivetrain->GetDefaultStrategy(), true);
-}
-void Robot::TeleopPeriodic() {
-=======
 	Schedule(drivetrain->GetDefaultStrategy(), true); // Use manual strategy
 	Schedule(climber->GetDefaultStrategy(), true);
->>>>>>> Connor/strats
 }
+void Robot::TeleopPeriodic() {}
 
 // Test Logic
 void Robot::TestInit() {}
