2ef26b6605cc3ae15f59737ce742728caa22473a
Anna <anna.pedersen@student.education.wa.edu.au>
2021-03-02 14:46:35 +0800

    changed deadzones

--- 4788/src/main/cpp/strategies/ShooterStrategy.cpp
+++ 4788/src/main/cpp/strategies/ShooterStrategy.cpp
@@ -8,9 +8,9 @@ ShooterManualStrategy::ShooterManualStrategy(std::string name, Shooter &shooter,
 }
 
 void ShooterManualStrategy::OnUpdate(double dt) {
-	double turretPower = _contGroup.Get(ControlMap::TurretTurn) > fabs(ControlMap::JoystickDeadZones) ? _contGroup.Get(ControlMap::TurretTurn) : 0;
+	double turretPower = _contGroup.Get(ControlMap::TurretTurn) > fabs(ControlMap::XboxDeadzone) ? _contGroup.Get(ControlMap::TurretTurn) : 0;
 	double FlyWheelPower = fabs(_contGroup.Get(ControlMap::SpinUp)) > ControlMap::TriggerDeadzone ? _contGroup.Get(ControlMap::SpinUp) : 0;
-	double hoodPower = _contGroup.Get(ControlMap::HoodLM) > fabs(ControlMap::JoystickDeadZone) ? _contGroup.Get(ControlMap::HoodLM) : 0;
+	double hoodPower = _contGroup.Get(ControlMap::HoodLM) > fabs(ControlMap::XboxDeadzone) ? _contGroup.Get(ControlMap::HoodLM) : 0;
 
 	if (_contGroup.Get(ControlMap::Fire)) {
 		_shooter.setFire();
